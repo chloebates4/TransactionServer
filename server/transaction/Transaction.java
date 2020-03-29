c4a753e27aa1aaab8a2ca80ced9ec800f40276d3
Colton Spector <39631194+Spector57@users.noreply.github.com>
2020-03-29 13:19:05 -0700

    Updated Transaction
    Added Getters & Setters

--- server/transaction/Transaction.java
+++ server/transaction/Transaction.java
@@ -1,5 +1,41 @@
 package server.transaction;
 
+import java.util.ArrayList;
+// Import Packages
 public class Transaction {
+
+  int transID;
+  ArrayList<Lock> locks = null;
+
+  StringBuffer log = new StringBuffer("");
+
+  Transaction(int transID) {
+    this.transID = transID;
+    this.locks = new ArrayList();
+  }
+
+  public int getID() {
+    return transID;
+  }
+
+  public ArrayList<lock> getLocks() {
+    return locks;
+  }
+
+  public void addLock(Lock lock) {
+    locks.add(lock);
+  }
+
+  public void log(String logString) {
+    log.append("\n").append(logString);
+
+    if(TransactionServer.transactionView) {
+      System.out.println("Transaction # " + this.getID() + ((this.getID() < 10) ? " " : " ") + logString);
+    }
+  }
+
+  public StringBuffer getLog() {
+    return log;
+  }
 
 }
